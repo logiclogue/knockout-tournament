7aec7899fa08835517473960b717374659e41d2d
logiclogue <jordanpaullord@hotmail.co.uk>
2017-08-14 13:03:50 +0100

    Updates Round with empty test

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,17 @@
-function Round() {
-    
+function Round(winnerLambda, matches) {
+    this.winnerLambda = winnerLambda;
+    this.matches = matches;
 }
 
 Round.prototype = {
 
+    getWinners: function () {
+        
+    },
 
+    forEach: function () {
+
+    }
     
 };
 
---
+++ test/Round-test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe("Round", function () {
+    describe("#getWinners()", function () {
+
+    });
+});
