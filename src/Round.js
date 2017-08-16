de2704f6cfb15bd1be61b084e544e5a6d2dc151f
logiclogue <jordanpaullord@hotmail.co.uk>
2017-08-16 13:12:34 +0100

    Adds losers and teams getters to Round

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,6 @@
-function Round(winnerLambda, matches) {
+function Round(winnerLambda, loserLambda, matches) {
     this.winnerLambda = winnerLambda;
+    this.loserLambda = loserLambda;
     this.matches = matches;
 }
 
@@ -7,6 +8,14 @@ Round.prototype = {
 
     get winners() {
         return this.matches.map(this.winnerLambda);
+    },
+
+    get losers() {
+        return this.matches.map(this.loserLambda);
+    },
+
+    get teams() {
+        return this.winners.concat(this.losers);
     }
     
 };
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,22 +2,57 @@ var expect = require('chai').expect;
 var Round = require('../src/Round');
 
 describe("Round", function () {
+    function Match(winner, loser) {
+        this.winner = winner;
+        this.loser = loser;
+    }
+
+    var matches = [
+        new Match("Derby", "Preston"),
+        new Match("Man Utd", "West Ham")
+    ];
+    
+    var getWinner = match => match.winner;
+    var getLoser = match => match.loser;
+
+    var round = new Round(getWinner, getLoser, matches);
+
     describe("#winners", function () {
-        it('should behave exactly as map', function () {
+        it('should return all the winners', function () {
             // arrange
-            var match = {
-                winner: 5
-            };
-            var matches = [match, match];
-            var winnerLambda = (match) => match.winner;
-            var round = new Round(winnerLambda, matches);
+            var expected = ["Derby", "Man Utd"];
 
             // act
             var result = round.winners;
-            var expectedResult = matches.map(winnerLambda);
 
             // assert
-            expect(result).to.deep.equal(expectedResult);
+            expect(result).to.deep.equal(expected);
+        });
+    });
+
+    describe('#losers', function () {
+        it('should return all the losers', function () {
+            // arrange
+            var expected = ["Preston", "West Ham"];
+
+            // act
+            var result = round.losers;
+
+            // assert
+            expect(result).to.deep.equal(expected);
+        });
+    });
+
+    describe('#teams', function () {
+        it('should return all the teams', function () {
+            // arrange
+            var expected = ["Derby", "Man Utd", "Preston", "West Ham"];
+
+            // act
+            var result = round.teams;
+
+            // assert
+            expect(result).to.deep.equal(expected);
         });
     });
 });
