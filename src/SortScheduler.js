54936ed8cc4b7ba38f49c444f555222a17f6602e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-08-27 21:39:59 +0100

    Adds empty SortScheduler

---
+++ src/SortScheduler.js
@@ -0,0 +1,15 @@
+// (Team -> Team -> Number) -> Scheduler
+function SortScheduler() {
+    
+}
+
+SortScheduler.prototype = {
+
+    // SortScheduler ~> [Team] -> [(Team, Team)]
+    schedule: function (teams) {
+        $
+    }
+    
+};
+
+module.exports = SortScheduler;
