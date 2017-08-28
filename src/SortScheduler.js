776d3ee2b92aa32097368881f0c2ca3c023cef24
logiclogue <jordanpaullord@hotmail.co.uk>
2017-08-28 09:38:24 +0100

    Adds completed sort scheduler

--- src/SortScheduler.js
+++ src/SortScheduler.js
@@ -1,13 +1,15 @@
+var sortSchedule = require('./sortSchedule');
+
 // (Team -> Team -> Number) -> Scheduler
-function SortScheduler() {
-    
+function SortScheduler(compareFunction) {
+    this.compareFunction = compareFunction;
 }
 
 SortScheduler.prototype = {
 
     // SortScheduler ~> [Team] -> [(Team, Team)]
     schedule: function (teams) {
-        $
+        return sortSchedule(this.compareFunction, teams);
     }
     
 };
---
+++ test/SortScheduler-test.js
@@ -0,0 +1,37 @@
+var expect = require('chai').expect;
+var SortScheduler = require('../src/SortScheduler');
+var stubs = require('./stubs');
+
+describe('SortScheduler', function () {
+    var scheduler = new SortScheduler(stubs.compareFunction);
+
+    describe('#schedule()', function () {
+        context('numbers and sort on size', function () {
+            it('should sort and pair them on size', function () {
+                // arrange
+                var numbers = [8, 6, 7, 4, 3, 0];
+                var expected = [[0, 3], [4, 6], [7, 8]];
+
+                // act
+                var result = scheduler.schedule(numbers);
+
+                // assert
+                expect(result).to.deep.equal(expected);
+            });
+        });
+
+        context('odd number of teams', function () {
+            it('should discard the lowest ranked team', function () {
+                // arrange
+                var numbers = [8, 6, 7, 4, 3, 0, 2];
+                var expected = [[0, 2], [3, 4], [6, 7]];
+
+                // act
+                var result = scheduler.schedule(numbers);
+
+                // assert
+                expect(result).to.deep.equal(expected);
+            });
+        });
+    });
+});
