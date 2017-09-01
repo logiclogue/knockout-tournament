95e79b7b39ce2e71facc011b635f268afce1f18a
logiclogue <jordanpaullord@hotmail.co.uk>
2017-09-01 22:48:31 +0100

    Renames all tests to not use the word should

--- test/sortSchedule-test.js
+++ test/sortSchedule-test.js
@@ -6,7 +6,7 @@ describe('sortSchedule()', function () {
     var compareFunction = stubs.compareFunction;
 
     context('numbers and sort on size', function () {
-        it('should sort and pair them on size', function () {
+        it('sorts and pair them on size', function () {
             // arrange
             var numbers = [8, 6, 7, 4, 3, 0];
             var expected = [[0, 3], [4, 6], [7, 8]];
@@ -20,7 +20,7 @@ describe('sortSchedule()', function () {
     });
 
     context('odd number of teams', function () {
-        it('should discard the lowest ranked team', function () {
+        it('discards the lowest ranked team', function () {
             // arrange
             var numbers = [8, 6, 7, 4, 3, 0, 2];
             var expected = [[0, 2], [3, 4], [6, 7]];
