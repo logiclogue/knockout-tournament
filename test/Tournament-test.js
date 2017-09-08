062ea8ae700234c95f4389dfa23083e7d424f8e4
logiclogue <jordanpaullord@hotmail.co.uk>
2017-09-08 22:13:23 +0100

    Updates test for Tournament, preparation

--- test/Tournament-test.js
+++ test/Tournament-test.js
@@ -1,13 +1,24 @@
 var expect = require('chai').expect;
 var Tournament = require('../src/Tournament');
+var RandomScheduler = require('../src/RandomScheduler');
+var stubs = require('./stubs');
 
 describe('Tournament', function () {
-    var tournament = new Tournament();
+    var scheduler = new RandomScheduler('');
+    var tournament = new Tournament(
+        stubs.getWinner,
+        stubs.getLoser,
+        stubs.createMatch,
+        (n) => scheduler,
+        stubs.teams
+    );
 
     describe('#getRound()', function () {
         context('0th round', function () {
             it('returns the first round', function () {
+                var result = tournament.getRound(0);
 
+                expect(result).to.equal();
             });
         });
     });
